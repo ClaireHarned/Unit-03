97b96afa64b0952fb68bc5fcf245b10620bf0a89
ClaireHarned <charned@wisc.edu>
2023-04-06 15:36:49 -0500

    Activity 9 update

--- Chapter 9/js/main.js
+++ Chapter 9/js/main.js
@@ -18,10 +18,10 @@ function setMap() {
     //projection
     var projection = d3
         .geoAlbers()
-        .center([0, 46.2])
-        .rotate([-2, 0, 0])
-        .parallels([43, 62])
-        .scale(2500)
+        .center([-6, 47])
+        .rotate([-20, 0, 0])
+        .parallels([42, 62])
+        .scale(1000)
         .translate([width / 2, height / 2]);
 
     var path = d3.geoPath().projection(projection);
@@ -30,17 +30,23 @@ function setMap() {
     var promises = [
         d3.csv("data/Countries.csv"),
         d3.json("data/EuropeCountries.json"),
+        d3.json("data/Countries.json"),
     ];
     Promise.all(promises).then(callback);
 
     function callback(data) {
         var csvData = data[0],
-            europe = data[1];
+            europe = data[1],
+            countries = data[2];
 
         //europe TopoJSON
-        var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries);
+        var europeCountries = topojson.feature(europe, europe.objects.pasted);
 
-        var graticule = d3.geoGraticule().step([5, 5]); //graticule lines every 5 degrees
+        //countries TopoJSON
+        var countries = topojson.feature(countries, countries.objects.CountriesTJSON);
+
+        //add in graticule lines every 5 degrees
+        var graticule = d3.geoGraticule().step([5, 5]);
 
         //create graticule background
         var gratBackground = map
@@ -56,13 +62,22 @@ function setMap() {
             .enter() //create an element for each datum
             .append("path") //append each element to the svg as a path element
             .attr("class", "gratLines") //assign class
-            .attr("d", path); 
+            .attr("d", path);
+
+        //add all countries to map
+        var countries = map
+            .append("path")
+            .datum(countries)
+            .attr("class", "countries")
+            .attr("d", path);
 
         //add Europe countries to map
         var europe = map
             .append("path")
             .datum(europeCountries)
-            .attr("class", "countries")
+            .attr("class", "europe")
             .attr("d", path);
+
+
     }
 }
